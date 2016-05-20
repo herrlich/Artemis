ec4aaac085ee894ea80b424501b62213335d7db0
herrlich <thanatos0124@gmail.com>
2016-05-19 22:45:58 -0400

    5.19

--- 110.Balanced Binary Tree.java
+++ 110.Balanced Binary Tree.java
@@ -9,20 +9,18 @@
  */
 public class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        return (check(root) != -1);
+        if (checkByDepth(root) == -1) return false;
+        return true;
     }
     
-    private int check(TreeNode root) {
-        if (root == null) return 0;
-        int left = check(root.left);
-        int right = check(root.right);
-        if (left == -1 || right == -1) return -1;
-        if (Math.abs(left - right) > 1) return -1;
-        return Math.max(left, right) + 1;
+    public int checkByDepth(TreeNode t) {
+        if (t == null) return 0;
+        int left = checkByDepth(t.left);
+        int right = checkByDepth(t.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+        else return Math.max(left, right) + 1;
     }
 }
-
 
 //Another method
 /**
--- 88.Merge Sorted Array.java
+++ 88.Merge Sorted Array.java
@@ -6,7 +6,21 @@ public class Solution {
             if (nums1[p1] > nums2[p2]) nums1[pos--] = nums1[p1--];
             else nums1[pos--] = nums2[p2--];
         }
-        while (p1 >= 0) nums1[pos--] = nums1[p1--];
         while (p2 >= 0) nums1[pos--] = nums2[p2--];
+    }
+}
+
+----------------------------------------------------------------------------
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) nums1[p1 + p2 + 1] = nums1[p1--];
+            else nums1[p1 + p2 + 1] = nums2[p2--];
+        }
+        while (p2 >= 0) {
+            nums1[p2] = nums2[p2];
+            --p2;
+        }
     }
 }
